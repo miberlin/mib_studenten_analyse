2f8451dfb83b0caa6a66f89208b494b994e6cd35
aeparilli <38952994+aeparilli@users.noreply.github.com>
2022-08-08 16:01:03 +0200

    TEST

--- app/main.py
+++ app/main.py
@@ -5,11 +5,7 @@ def main():
 
     # Them  config streamlit
     streamlit.set_page_config(layout='wide',
-                                  menu_items={
-                                      'Get Help': None,
-                                      'Report a bug': None,
-                                      'About': None
-                                  }
+                                  menu_items={}
                               )
     # read the configuration file and initialize random generators
     cfg = read_config('/app/mib_studenten_analyse/app/config/config.yaml')
