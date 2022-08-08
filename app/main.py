5d4044765c0d10c1bb9d16f95808c71be25d4d14
aeparilli <38952994+aeparilli@users.noreply.github.com>
2022-08-08 14:51:49 +0200

    TEST

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def main():
     # Select value range for courses
     streamlit.dataframe(all_dates_kurse['MiB-Kurs-Name'])
     streamlit.write(type(kurs_id))
-    course_dates = all_dates_kurse[all_dates_kurse['MiB-Kurs-Name'] == "Kurs Berlin"]
+    course_dates = all_dates_kurse[all_dates_kurse['MiB-Kurs-Name'] == ["Kurs Berlin", 'Kurs Aachen']]
     course_dates = course_dates['Datum-df']
     min_date_kurs, max_date_kurs = min_max_dates(course_dates)
 
