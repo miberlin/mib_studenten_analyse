fa7a5caeb561e6fe1e5016a19dd18bc627781b62
aeparilli <38952994+aeparilli@users.noreply.github.com>
2022-09-12 15:33:43 +0200

    Cleaning and modification of  the columns names

--- app/utils.py
+++ app/utils.py
@@ -84,7 +84,7 @@ def list_airtable_records(base, table, cfg):
             df[cat] = df[cat].str[0]
         for cat in table_dict['clean_nan']:
             streamlit.dataframe(df[cat])
-            df[cat] = df[cat].astype('float64')
+            df[cat] = df[cat]#.astype('float64')
     elif table_name == "PK Ergebnisse":
         for cat in table_dict['to_clean']:
             df[cat] = df[cat].str[0]
